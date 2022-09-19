5a6e8bf4f4cf7a208ec2dd52b224ea0a40353296
Yudhistiro Agung <yudhistiro.agung@gmail.com>
2022-09-19 19:25:49 +0700

    add some actions

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,9 +1,26 @@
 describe('empty spec', () => {
   it('passes', () => {
-    cy.visit('http://localhost:3000/')
+    // open the app
+    cy.visit('https://github.com');
 
-    cy.get('#field-\:r1\:').type('Automate using cypress')
+    // click on hamburger menu
+    cy.get('.text-right > .js-details-target')
+      .click();
 
-    cy.get('Add Todo').click()
+    // type jake wharton on search bar input
+    cy.get('.header-search-wrapper > .form-control')
+      .type('jake wharton{enter}');
+
+    // scroll to bottom page
+    cy.scrollTo(0, 1000);
+
+    // click on repo nmwilk/soms-jw
+    cy.get('.col-md-9 > div.px-2')
+      .contains('nmwilk/soms-jw')
+      .click()
+
+    // click on app pakage on project
+    cy.get(':nth-child(2) > .col-md-2 > .css-truncate > .js-navigation-open')
+      .click()
   })
 })
